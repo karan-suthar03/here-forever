e4380cf70655bcecd027e42073b7fc2a0e7b843b
karan-suthar03 <116728069+karan-suthar03@users.noreply.github.com>
2023-03-22 23:18:18 +0530

    Update script.js

--- kiki/script.js
+++ kiki/script.js
@@ -14,6 +14,8 @@ const db = firebase.firestore();
 const messageContainer = document.querySelector('.messages');
 const userRef = db.collection('users').doc(username);
 const dmsgs = firebase.database().ref("msgs");
+const time1 = firebase.database().ref("time1");
+const time2 = firebase.database().ref("time2");
 dmsgs.set("");
 userRef.get().then((doc) => {
     if (doc.exists) {
@@ -21,7 +23,8 @@ userRef.get().then((doc) => {
             snapshot.docChanges().forEach((change) => {
                 if (change.type === 'added') {
                     const message = change.doc.data();
-                    const messageElement = document.createElement('li');
+                    const messageElement = document.createElement('div');
+                    const messageElementone = document.createElement('div');
                     const textElement = document.createElement('div');
                     const textNode = document.createTextNode(message.message);
                     const avatarElement = document.createElement('div');
@@ -46,7 +49,8 @@ userRef.get().then((doc) => {
                     messageElement.appendChild(avatarElement);
                     textWrapperElement.appendChild(textElement);
                     messageElement.appendChild(textWrapperElement);
-                    messageContainer.appendChild(messageElement);
+                    messageElementone.appendChild(messageElement);
+                    messageContainer.appendChild(messageElementone);
                     messageContainer.scrollTop = messageContainer.scrollHeight;
                 }
             });
@@ -81,5 +85,56 @@ sendMessageButton.addEventListener('click', () => {
             timestamp: firebase.firestore.FieldValue.serverTimestamp()
         });
         messageInput.value = '';
+        messageInput.focus();
+    }
+});
+addEmojis(['💕', '❤️', '😚', '😌', '😭', '🤣', '🥺', '😗', '🌚', '🙂', '😔', '💋', '🤤', '😩', '😝', '🍑', '🍒', '🤜', '🤛', '😍', '🥵', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '😀', '😃', '😄', '😁', '😆', '😅', '😂', '🤣', '☺️', '😊', '😇', '🙂', '🙃', '😉', '😌', '😍', '🥰', '😘', '😗', '😙', '😚', '😋', '😛', '😝', '😜', '🤪', '🤨', '🧐', '🤓', '😎', '🥸', '🤩', '🥳', '😏', '😒', '😞', '😔', '😟', '😕', '🙁', '☹️', '😣', '😖', '😫', '😩', '🥺', '😢', '😭', '😮\u200d💨', '😤', '😠', '😡', '🤬', '🤯', '😳', '🥵', '🥶', '😱', '😨', '😰', '😥', '😓', '🫣', '🤗', '🫡', '🤔', '🫢', '🤭', '🤫', '🤥', '😶', '😶\u200d🌫️', '😐', '😑', '😬', '🫠', '🙄', '😯', '😦', '😧', '😮', '😲', '🥱', '😴', '🤤', '😪', '😵', '😵\u200d💫', '🫥', '🤐', '🥴', '🤢', '🤮', '🤧', '😷', '🤒', '🤕', '🤑', '🤠', '😈', '👿', '👹', '👺', '🤡', '💩', '👻', '💀', '☠️', '👽', '👾', '🤖', '🎃', '😺', '😸', '😹', '😻', '😼', '😽', '🙀', '😿', '😾', '👋', '🤚', '🖐', '✋', '🖖', '👌', '🤌', '🤏', '✌️', '🤞', '🤟', '🤘', '🤙', '👈', '👉', '👆', '🖕', '👇', '☝️', '👍', '👎', '✊', '👊', '🤛', '🤜', '👏', '🙌', '👐', '🤲', '🤝', '🙏', '✍️', '💅', '🤳', '💪', '🦾', '🦵', '🦿', '🦶', '👣', '👂', '🦻', '👃', '🫀', '🫁', '🧠', '🦷', '🦴', '👀', '👁', '👅', '👄', '💋', '🩸', '🐶', '🐱', '🐭', '🐹', '🐰', '🦊', '🐻', '🐼', '🐻\u200d❄️', '🐨', '🐯', '🦁', '🐮', '🐷', '🐽', '🐸', '🐵', '🙈', '🙉', '🙊', '🐒', '🐔', '🐧', '🐦', '🐤', '🐣', '🐥', '🦆', '🦅', '🦉', '🦇', '🐺', '🐗', '🐴', '🦄', '🐝', '🪱', '🐛', '🦋', '🐌', '🐞', '🐜', '🪰', '🪲', '🪳', '🦟', '🦗', '🕷', '🕸', '🦂', '🐢', '🐍', '🦎', '🦖', '🦕', '🐙', '🦑', '🦐', '🦞', '🦀', '🪸', '🐡', '🐠', '🐟', '🐬', '🐳', '🐋', '🦈', '🐊', '🐅', '🐆', '🦓', '🦍', '🦧', '🦣', '🐘', '🦛', '🦏', '🐪', '🐫', '🦒', '🦘', '🦬', '🐃', '🐂', '🐄', '🐎', '🐖', '🐏', '🐑', '🦙', '🐐', '🦌', '🐕', '🐩', '🦮', '🐕\u200d🦺', '🐈', '🐈\u200d⬛', '🪶', '🐓', '🦃', '🦤', '🦚', '🦜', '🦢', '🦩', '🕊', '🐇', '🦝', '🦨', '🦡', '🦫', '🦦', '🦥', '🐁', '🐀', '🐿', '🦔', '🐾', '🐉', '🐲', '🌵', '🎄', '🌲', '🌳', '🌴', '🪹', '🪺', '🪵', '🌱', '🌿', '☘️', '🍀', '🎍', '🪴', '🎋', '🍃', '🍂', '🍁', '🍄', '🐚', '🪨', '🌾', '💐', '🌷', '🪷', '🌹', '🥀', '🌺', '🌸', '🌼', '🌻', '🌞', '🌝', '🌛', '🌜', '🌚', '🌕', '🌖', '🌗', '🌘', '🌑', '🌒', '🌓', '🌔', '🌙', '🌎', '🌍', '🌏', '🪐', '💫', '⭐️', '🌟', '✨', '⚡️', '☄️', '💥', '🔥', '🌪', '🌈', '☀️', '🌤', '⛅️', '🌥', '☁️', '🌦', '🌧', '⛈', '🌩', '🌨', '❄️', '☃️', '⛄️', '🌬', '💨', '💧', '💦', '🫧', '☔️', '☂️', '🌊', '🌫', '🍏', '🍎', '🍐', '🍊', '🍋', '🍌', '🍉', '🍇', '🍓', '🫐', '🍈', '🍒', '🍑', '🥭', '🍍', '🥥', '🥝', '🍅', '🍆', '🥑', '🥦', '🥬', '🥒', '🌶', '🫑', '🌽', '🥕', '🫒', '🧄', '🧅', '🥔', '🍠', '🥐', '🥯', '🍞', '🥖', '🥨', '🧀', '🥚', '🍳', '🧈', '🥞', '🧇', '🥓', '🥩', '🍗', '🍖', '🦴', '🌭', '🍔', '🍟', '🍕', '🫓', '🥪', '🥙', '🧆', '🌮', '🌯', '🫔', '🥗', '🥘', '🫕', '🥫', '🍝', '🍜', '🍲', '🍛', '🍣', '🍱', '🥟', '🦪', '🍤', '🍙', '🍚', '🍘', '🍥', '🥠', '🥮', '🍢', '🍡', '🍧', '🍨', '🍦', '🥧', '🧁', '🍰', '🎂', '🍮', '🍭', '🍬', '🍫', '🍿', '🍩', '🍪', '🌰', '🥜', '🍯', '🥛', '🍼', '🫖', '☕️', '🍵', '🧃', '🥤', '🧋', '🍶', '🍺', '🍻', '🥂', '🍷', '🥃', '🍸', '🍹', '🧉', '🍾', '🧊', '🥄', '🍴', '🍽', '🥣', '🥡', '🥢', '🧂', '❤️', '🧡', '💛', '💚', '💙', '💜', '🖤', '🤍', '🤎', '❤️\u200d🔥', '❤️\u200d🩹', '💔', '❣️', '💕', '💞', '💓', '💗', '💖', '💘', '💝']);
+document.getElementById('bahar').classList.add('hidden');
+
+function addEmojis(emojis) {
+    const drawer = document.getElementById('drawer');
+    for (const emoji of emojis) {
+        const emojiDiv = document.createElement('div');
+        emojiDiv.classList.add('emoji');
+        emojiDiv.innerHTML = emoji;
+        emojiDiv.onclick = () => addEmoji(emoji);
+        drawer.appendChild(emojiDiv);
+    }
+}
+
+function addEmoji(emoji) {
+    let input = document.getElementById('input');
+    let cursorPosition = input.selectionStart; // get current cursor position
+    input.value = input.value.slice(0, cursorPosition) + emoji + input.value.slice(cursorPosition); // insert emoji at cursor position
+    cursorPosition += emoji.length; // update cursor position
+    input.setSelectionRange(cursorPosition, cursorPosition); // set cursor position to end of input field
+    input.focus(); // set focus back to input field
+}
+
+function toggleEmojiDrawer() {
+    drawer.style.pointerEvents = 'auto';
+    if (drawer.classList.contains('hidden')) {
+        drawer.classList.remove('hidden');
+        document.getElementById('bahar').classList.remove('hidden');
+    } else {
+        drawer.classList.add('hidden');
+        document.getElementById('bahar').classList.add('hidden');
+        drawer.style.pointerEvents = 'none';
+    }
+    document.getElementById('input').focus();
+}
+if (drawer.classList.contains('hidden')) {
+    drawer.style.pointerEvents = 'none';
+}
+let emojiButton = document.getElementById('emoji-button');
+document.addEventListener('click', function(event) {
+    let baharDiv = document.getElementById('bahar');
+    let targetElement = event.target;
+    if (!baharDiv.contains(targetElement) && !emojiButton.contains(targetElement.closest('.ok'))) {
+        // user clicked outside of the #bahar div and the clicked element is not an emoji button or its child element, run your specific function here
+        console.log('User clicked outside of the #bahar div');
+        drawer.classList.add('hidden');
+        document.getElementById('bahar').classList.add('hidden');
+        drawer.style.pointerEvents = 'none';
     }
 });
